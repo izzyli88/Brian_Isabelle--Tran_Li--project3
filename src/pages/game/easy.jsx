40cb0669b83db558cf310c6142bf42a3db39ca3b
izzyli88 <li.isab@northeastern.edu>
2025-03-24 11:09:24 -0700

    project 2 writeup

--- src/pages/game/easy.jsx
+++ src/pages/game/easy.jsx
@@ -3,14 +3,12 @@ import "../../styles/styles.css";
 import { GameProvider } from "../../context/GameContext";
 import Board from "../../components/Board";
 import Timer from "../../components/Timer";
-import { NavLink } from "react-router-dom";
 import { TimeProvider } from "../../context/TimerContext";
 
 export default function EasyMode() {
   return (
     <>
       <TimeProvider>
-        <NavLink to="/game/easy"></NavLink>
         <GameProvider mode="easy">
           <div className="game-container">
             <h1>Easy Mode</h1>
